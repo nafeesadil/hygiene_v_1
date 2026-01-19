d6acf03a19d601dc1bbc8999216fa62533f0a651
nafeesadil <nafeesmohammadadil@gmail.com>
2026-01-19 09:19:12 +0100

    Implement TasksPage with state management and add TaskCard widget for task display

--- lib/features/tasks/tasks.dart
+++ lib/features/tasks/tasks.dart
@@ -1,16 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:hygiene_v_1/features/tasks/widgets/task_cards.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class TasksPage extends StatelessWidget {
+class TasksPage extends StatefulWidget {
   const TasksPage({super.key});
 
   @override
+  State<TasksPage> createState() => _TasksPageState();
+}
+
+class _TasksPageState extends State<TasksPage> {
+  final _controller = PageController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Tasks'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: [
+                  Text(
+                    'My ',
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+                  Text('Tasks', style: TextStyle(fontSize: 28)),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 25),
+            // ignore: sized_box_for_whitespace
+            Container(
+              height: 200,
+              child: PageView(
+                controller: _controller,
+                scrollDirection: Axis.horizontal,
+                children: [TaskCard(), TaskCard(), TaskCard()],
+              ),
+            ),
+
+            SizedBox(height: 25),
+
+            SmoothPageIndicator(
+              controller: _controller,
+              count: 3,
+              effect: ExpandingDotsEffect(activeDotColor: Colors.grey.shade800),
+            ),
+
+            SizedBox(height: 25),
+
+            Column(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.child_friendly_rounded,
+                    color: const Color.fromARGB(255, 144, 151, 144),
+                  ),
+                  title: Text('Task 1'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.check_box, color: Colors.green),
+                  title: Text('Task 2'),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.check_box_outline_blank,
+                    color: Colors.grey,
+                  ),
+                  title: Text('Task 3'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      body: const Center(child: Text('This is the Tasks Page')),
     );
   }
 }
---
+++ lib/features/tasks/widgets/task_cards.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatelessWidget {
+  const TaskCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Container(
+        width: 300,
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 181, 122, 252),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 10),
+                Text(
+                  'Daily Tasks',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Progress bar go here',
+                  style: TextStyle(fontSize: 15, color: Colors.white),
+                ),
+
+                SizedBox(height: 10),
+              ],
+            ),
+            Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'details',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      '  3/10',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
